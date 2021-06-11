cc7c312b878240e9b67686b73e9d1b3a2fa27506
Joe Hopkinson <joe@hopkinson.co.uk>
2021-06-11 18:13:30 +0000

    Delete ethz directory

--- ethz/workflow/scripts/dumpcap.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-dumpcap -w /home/joe/catkin_ws/src/data/dumpcap/data -f "host turtlebot"
-
--- ethz/workflow/shell_scripts/coverage_server.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-/opt/ros/melodic/bin/roslaunch polygon_coverage_ros coverage_planner.launch
